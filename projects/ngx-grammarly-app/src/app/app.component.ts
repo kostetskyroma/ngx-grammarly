20bdd6a1074ebdd872ddece98f4cc4967cadf776
Roman Kostetskyi <kostetskyroma@gmail.com>
2022-10-05 19:46:36 +0300

    app: add plugin examples

--- projects/ngx-grammarly-app/src/app/app.component.ts
+++ projects/ngx-grammarly-app/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { demoText } from './app.constant';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ngx-grammarly-app';
+  public readonly demoText = {
+    textarea: demoText.textarea,
+    input: demoText.input,
+    contenteditable: demoText.contenteditable,
+  };
 }
